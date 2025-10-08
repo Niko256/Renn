27cc267bacbc2165085e4613240cfd0e3201b649
Niko256 <kolbnickolay@yandex.ru>
2025-10-08 04:00:10 +0300

    initial structure...

--- include/Core/Concepts.hpp
+++ include/Core/Concepts.hpp
@@ -1,4 +1,9 @@
 #pragma once
 
-we want to be sure that object Func
-    could be invoked with pack of arguments Args...
+#include <concepts>
+
+// we want to be sure that object Func could be invoked with pack of arguments Args...
+template <typename Func, typename... Args>
+concept InvokableWith = requires(Func&& f, Args&&... args) {
+    { f(args...) };
+};
